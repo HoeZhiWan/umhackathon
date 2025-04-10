8c366e549756078eceecef85fbc8ae376795da03
Shin <shinyeongbeh@gmail.com>
2025-04-10 23:57:33 +0800

    Added best selling day function

--- app/sql/page.tsx
+++ app/sql/page.tsx
@@ -5,6 +5,7 @@ import { supabase } from "../actions/sql";
 import { topSellingItemsWeek } from "../actions/sql"
 import { topSellingItemsMonth } from "../actions/sql"
 import { bestSellingTime } from "../actions/sql"
+import { bestSellingDay } from "../actions/sql"
 
 
 export default function sqlPage() {
@@ -34,6 +35,15 @@ export default function sqlPage() {
                 className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-700 focus:ring-opacity-75 cursor-pointer"
             >
                 Best Selling Time
+            </button>
+            <button 
+                onClick={async () => {
+                    const result = await bestSellingDay('5b8d2');
+                    console.log(result);
+                }}
+                className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-700 focus:ring-opacity-75 cursor-pointer"
+            >
+                Best Selling Day
             </button>
         </>
     );
